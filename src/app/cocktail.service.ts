85dc9fb8b1635b8cd2ab45df39cfddb7c789c239
clauga <guerrero.ayala.cs@gmail.com>
2023-09-27 11:58:27 +0200

    challenge 11

---
+++ src/app/cocktail.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Cocktail } from './cocktail.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CocktailService {
+  // public cocktailsUrl = 'assets/cocktails.json';
+
+  constructor(public http: HttpClient) { }
+
+  getCocktails(): Observable<Cocktail[]> {
+    return this.http.get<Cocktail[]>("assets/cocktails.json");
+  }
+}
